bbd1f65cbce41e8f722fae5e9c306c9dbe3f1e8c
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-08-05 14:50:38 +0900

    isDoorLockedを.pyだけでなんとかなるようにした

--- IsDoorLocked/run_notify_sh.py
+++ IsDoorLocked/run_notify_sh.py
@@ -1,5 +1,9 @@
 import serial
-import subprocess
+import requests
+import json
+
+# WebフックURLを設定
+WEBHOOK_URL = "https://kosenjp.webhook.office.com/webhookb2/8aaa7a61-f810-441c-a4fe-faa644a1204c@72fe835d-5e95-4512-8ae0-a7b38af25fc8/IncomingWebhook/a8ee212863534604ba3ba6fc9976a77c/101ee8dd-06c6-445c-aa21-c4293aebba40"
 
 # ESP32とのシリアルポートを開く
 ser = serial.Serial("/dev/ttyUSB0", 9600)  # '/dev/ttyUSB0'は適切なポートに置き換える
@@ -10,6 +14,12 @@ while True:
         data = ser.readline().decode().strip()
         print("Received:", data)
 
-        # 受信したデータが指定された命令ならば、シェルスクリプトを実行
+        # 受信したデータが指定された命令ならば、WebフックにPOSTリクエストを送信
         if data == "the key was Locked!!":
-            subprocess.run(["./notice_post.sh"])
+            payload = {"text": "the key was locked!!"}
+            headers = {"Content-Type": "application/json"}
+            response = requests.post(
+                WEBHOOK_URL, headers=headers, data=json.dumps(payload)
+            )
+            print("Response status code:", response.status_code)
+            print("Response body:", response.text)
